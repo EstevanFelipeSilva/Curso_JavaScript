481c59bb2ddf948247b3c8c2ba05e7e5752ccbf5
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-05 20:01:07 -0300

    Declarando variável com ( LET ) no JavaScript
    Hoje vimos um pouco sobre como usar a Palavra-Chave ( LET ) e algumas regras para melhor aproveitamento de seu uso.

--- Aula-03/Aula-03.js
+++ Aula-03/Aula-03.js
@@ -46,3 +46,40 @@
         com o último valor declarado para ( x ), pois ( VAR ) tem um escopo Global e permite
         que seu valor seja modificado infinitas vezes.
 */
+
+/*
+                    ESCOPO DO BLOCO, OU EM BLOCO, COMO PREFERIR.
+
+        * Antes da Atualização ES6 (2015), JavaScript só tinha o Escopo Global e o Escopo de Função.
+
+        * O ES6 introduziu duas novas Palavras-Chaves ao JavaScript: ( LET ) e ( CONST ).
+
+        * Essas duas novas Palavras-Chaves ao JavaScript fornecem Block Scope.
+
+        * Variáveis declaradas dentro de um Bloco {} não podem ser acessadas de fora do Bloco.
+
+
+                    VEJA O TESTE QUE IREMOS FAZER ABAIXO COM UM CÓDIGO
+                    PARA TESTE:
+*/
+
+{
+    let x = 10;
+}
+console.log(x); /* AQUI ELE NÃO SERÁ CAPAZ DE RETORNAR O VALOR RECLARADO NA VARIÁVEL */
+
+/*
+        * Observando o código JavaScript acima, veja que declaramos a Variável dentro de um Bloco
+        indicando que o valor de ( x ) é igual a ( 10 ).
+
+        * Porém observe também que fizemos a chamada com o ( console.log(x); ) fora do Escopo do Bloco,
+        ao executar esse ( console.log() ), não irá aparecer nada em nosso console do terminal.
+
+        * Esse ( console.log(); ) só será capaz de retornar o valor de ( x ) depois que nós o
+        inserir dentro do Escopo do Bloco, da seguinte forma, confira abaixo.
+*/
+
+{
+    let z = 10;
+    console.log(z); /* AQUI ELE RETORNARÁ O VALOR ( 10 ) */
+}
