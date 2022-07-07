447d9f8102d793c9a59b18b68c20045fea48e65e
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-06 21:52:33 -0300

    Declarando variável com ( LET ) no JavaScript
    Hoje vimos um pouco sobre como usar a Palavra-Chave ( LET ) e algumas regras para melhor aproveitamento de seu uso.

--- Aula-03/Aula-03.js
+++ Aula-03/Aula-03.js
@@ -66,7 +66,7 @@
 {
     let x = 10;
 }
-console.log(x); /* AQUI ELE NÃO SERÁ CAPAZ DE RETORNAR O VALOR RECLARADO NA VARIÁVEL */
+console.log(x); /* AQUI ELE NÃO SERÁ CAPAZ DE RETORNAR O VALOR DECLARADO NA VARIÁVEL */
 
 /*
         * Observando o código JavaScript acima, veja que declaramos a Variável dentro de um Bloco
@@ -83,3 +83,40 @@ console.log(x); /* AQUI ELE NÃO SERÁ CAPAZ DE RETORNAR O VALOR RECLARADO NA VA
     let z = 10;
     console.log(z); /* AQUI ELE RETORNARÁ O VALOR ( 10 ) */
 }
+
+/* 
+                                REDECLARANDO A VARIÁVEL
+
+        * Redeclarando uma variável usando a Palavra-Chave ( VAR ) pode nos causar Problemas.
+
+        * Pois devemos lembrar que ( VAR ) tem escopo Global, isso significa que ao Redeclarar
+                a variável de dentro do Bloco, a variável fora do Bloco também será modificada.
+
+        OBSERVE O CÓDIGO ABAIXO, E SEUS COMENTÁRIOS;
+*/
+
+var e = 95; // AQUI A VARIÁVEL ESTARÁ RETORNANDO O VALOR ( 95 );
+
+{
+        var e = 17; // AQUI A VARIÁVEL FOI ALTERADA E PASSARÁ A RETORNAR O VALOR ( 17 );
+}
+
+console.log(e); // AQUI A VARIÁVEL MANTERÁ O VALOR ( 17 );
+
+/*
+        * Esse comportamento Global da Palavra-Chave ( VAR ), se torna um problema diante do
+                Desenvolvimento de uma Aplicação Real;
+
+        * Porém esse problema pode facilmente ser corrigido fazendo uso da Palavra-Chave ( LET );
+
+        OBERVE O CÓDIGO ABAIXO, E SEUS COMENTÁRIOS;        
+*/
+
+let f = 17; // AQUI NO ESCOPO GLOBAL A VARIÁVEL ( f ) TERÁ O VALOR ( 17 );
+
+{
+        let f = 95; // AQUI NESTE ESCOPO DENTRO DO BLOCO, ( f ) PASSARÁ A TER O VALOR ( 95 );
+        console.log(f); // ENQUANTO ( f ) FOR IMPRESSO DENTRO DESTE BLOCO ELE TERÁ SEU VALOR COMO ( 95 );
+}
+
+console.log(f); // AO RETORNAR AO ESCOPO GLOBAL ( f ), VOLTA A IMPRIMIR O VALOR ( 17 );
