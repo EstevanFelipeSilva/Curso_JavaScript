dc62641a0d24a27251724efc2c5d788e89002ffd
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-23 05:27:27 -0300

    TIPOS DE DADOS
    Entendendo os Tipos de Dados com os quais JavaScript trabalha.

---
+++ Aula-06/Aula-06-Exemplo-01.js
@@ -0,0 +1,19 @@
+/*
+            TIPOS DE DADOS
+
+    let a = 16;                         // NUMBER
+    let firstName = "Jhon";             // STRING
+    let x = {Name: "John", Age: 36};    // OBJECT
+
+
+            O CONCEITO DE TIPOS DE DADOS
+
+    No estudo da Programação, os Tipos de Dados são um
+        conceito muito importante.
+
+    Para podermos operar em variáveis, é importante que saibamos
+        o Tipo de variável que ela é.
+
+    Sem tipos de dados, o computador não pode resolver
+        os respectivos problemas com segurança.
+*/
