0022745fc8485d1ce65f9afd963d4281f33f645e
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-22 23:43:36 -0300

    OPERADORES
    Resolvendo Expressões Aritiméticas e vendo Operadores.

---
+++ Aula-05/Aula-05-Exemplo-01.js
@@ -0,0 +1,28 @@
+/*
+            OPERADORES EM JAVASCRIPT
+*/
+
+let x = 5;          // value 5;
+let y = 10;         // value 10;
+let z = x + y;      // value 15;
+
+/*
+        O operador de Atribuição ( = ) é utilizado
+        para atribuir um valor a variável;
+*/
+
+let a = 1;          // value 1;
+
+/*
+        O operador de ADIÇÃO ( + ) é utilizado
+        para somar o valor de duas Variáveis;
+*/
+
+let b = 1 + 4;      // valeu 5;
+
+/*
+        O operador de MULTIPLICAÇÃO ( * ) é utilizado
+        para multiplicar números;
+*/
+
+let c = 2 * 10;     // valeu 20;
