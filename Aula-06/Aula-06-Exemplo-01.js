cfabc955b41412c32ac196a2fce683315cadff61
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-24 20:17:07 -0300

    TIPOS DE DADOS
    Nesta Aula iremos ver os Tipos de Dados em JavaScript, e alguns comportamentos que ele apresentam.

--- Aula-06/Aula-06-Exemplo-01.js
+++ Aula-06/Aula-06-Exemplo-01.js
@@ -17,3 +17,48 @@
     Sem tipos de dados, o computador não pode resolver
         os respectivos problemas com segurança.
 */
+
+let a = 02 + " = " + "Fevereiro";
+
+console.log(a); // 2 = Fevereiro;
+
+/*
+    Observando a expressão acima faz algum sentido juntar
+        um ( Number ) a uma ( String )? Pela Lógica não
+        essa expressão ou Produzirá um Error ou Produzirá
+        um resultado insatisfatório;
+
+    Ao adicionar essa expressão o JavaScript tratará o Número
+        como ele sendo uma String e assim ele imprimirá as
+        informações todas empilhadas;
+*/
+
+let b = 02 + 17 + "Desenvolvedor";
+
+console.log(b); // 19Desenvolvedor;
+
+/*
+    JavaScript avalia expressões sempre dá Esquerda para a Direita.
+
+    Sequências diferentes iram produzir resultados diferentes.
+
+    No Exemplo acima, JavaScript nos trará o seguinte resultado:
+
+            19Desenvolvedor;
+    
+    Pois o JavaScript entenderá que os 2 primeiros tipos de dados
+        são do tipo ( Number ) então ele tratará como uma expressão
+        númerica e depois ele entende que o dado do tipo ( String )
+        está sendo somada aos números.
+*/
+
+let c = 'Desenvolvedor' + 02 + 17;
+
+console.log(c); // Desenvolvedor217;
+
+/*
+    Já no exemplo acima o JavaScript entende dos os dados descritos
+        dentro da variável, como sendo do tipo de dado ( String );
+
+    E assim quando imprimido o JS empilhará os dados;
+*/
