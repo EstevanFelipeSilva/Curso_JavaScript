8ab22e4593d53a8760167e4197fcdfddf80b379e
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-24 23:17:57 -0300

    TIPOS DE DADOS
    Nesta Aula iremos ver os Tipos de Dados em JavaScript, e alguns comportamentos que ele apresentam.

---
+++ Aula-06/Aula-06-Exemplo-03.js
@@ -0,0 +1,28 @@
+/*
+            ARRAY EM JAVASCRIPT
+
+    Os ARRAY EM JAVASCRIPT são escritos com colchetes [];
+
+    Os itens dentro do ARRAY são separados por virgula;
+
+    O código a seguir declara (cria) um ARRAY chamado (carros),
+        contendo dentro de si três itens.
+*/
+
+const arrayString = ['Um', 'Dois', 'Três']; // ['Um', 'Dois', 'Três];
+
+console.log(arrayString);
+
+const arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(arrayNumber);
+
+/*
+    Os índices dos ARRAY são baseados em Zero, isso nos indica que
+        o primeiro elemento (item) do array tem o índice [0];
+        o segundo elemento (item) do array tem o índice [1];
+        o terceiro elemento (item) do array tem o índice [2];
+        E assim por diante;
+
+    Veremos isso com mais profundidade no decorrer dos nossos Estudos;
+*/
