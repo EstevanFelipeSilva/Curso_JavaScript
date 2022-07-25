c28c44e6d2c5197e1c37e679aac88dad97ce4ec6
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-24 23:59:39 -0300

    TIPOS DE DADOS
    Nesta Aula iremos ver os Tipos de Dados em JavaScript, e alguns comportamentos que ele apresentam.

---
+++ Aula-06/Aula-06-Exemplo-04.js
@@ -0,0 +1,25 @@
+/*
+            OBJECTOS EM JAVASCRIPT
+
+    Os OBJECTOS EM JAVASCRIPT são escritos com {};
+
+    As propriedades do OBJECTO são escrtitas em pares como
+        nome:valeu, separadas por virgula;
+*/
+
+const Person = 
+{firstName:'Estevan', lastName:'Felipe', age:27, eyeColor:'Black'}; // EXEMPLO DE OBJECT JAVASCRIPT;
+
+console.log(Person);
+
+/*
+    O OBJECT (Person) visto acima tem 4 Propriedades:
+        
+        --- {firstName};
+        --- {lastName};
+        --- {eyeColor};
+        --- {age};
+        
+    Aprenderemos mais sobre como utilizar e manipular OBJECTOS JAVASCRIPT
+        no decorrer dos nossos Estudos;
+*/
