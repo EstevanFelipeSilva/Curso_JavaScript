e26137e5ec32bb4cbe1f54e5eed463a43ea37ef6
EstevanFelipeSilva <107445498+EstevanFelipeSilva@users.noreply.github.com>
2022-07-25 00:33:38 -0300

    TIPOS DE DADOS
    Nesta Aula iremos ver os Tipos de Dados em JavaScript, e alguns comportamentos que ele apresentam.

---
+++ Aula-06/Aula-06-Exemplo-05.js
@@ -0,0 +1,21 @@
+/*
+            O OPERADOR TYPEOF
+
+    Você pode usar o operador (TYPEOF) em JAVASCRIPT para localizar
+        o tipo de uma variável no JAVASCRIPT;
+
+    O OPERADOR TYPEOF retornará o tipo de uma variável ou expressão;
+*/
+
+let palavra1 = '';      // return (STRING);
+let palavra2 = 'Dev';   // return (STRING);
+let palavra3 = "";      // return (STRING);
+let palavra4 = "Dev";   // return (STRING);
+
+let numero1 = 0;        // return (NUMBER);
+let numero2 = 321;      // return (NUMBER);
+let numero3 = 321.123;  // return (NUMBER);
+let numero4 = (5);      // return (NUMBER);
+let numero5 = (5 + 5);  // return (NUMBER);
+
+// APRENDEREMOS MAIS SOBRE ( TYPEOF ) POSTERIORMENTE NESTE ESTUDO;
---
+++ Aula-06/Aula-06-Exemplo-06.js
@@ -0,0 +1,26 @@
+/*
+            INDEFINIDO EM JAVASCRIPT
+
+    Em JAVASCRIPT, uma variável sem valor tem o valor de (UNDEFINED);
+
+    O seu TIPO também é (UNDEFINED);
+*/
+
+let carro; // valor é (UNDEFINED), tipo também (UNDEFINED);
+
+/*
+    Qualquer variável pode ser esvaziada, definindo o seu valor como
+        (UNDEFINED). Logo seu TIPO também será (UNDEFINED);
+*/
+
+carros = undefined; // valor e tipo são (UNDEFINED);
+
+/*
+            VALORES VAZIOS
+
+    Um valor vazio não tem nada a ver com (UNDEFINED);
+
+    Uma (STRING) vazia tem um valor valido por exemplo e um TIPO;
+*/
+
+let car = ""; // o valor é ""; o TYPEOF é (STRING);
